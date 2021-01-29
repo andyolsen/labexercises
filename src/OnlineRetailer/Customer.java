0ddd9628e141e50c541010320fc002ba66b6394c
Andy Olsen <andyo@olsensoft.com>
2021-01-29 07:19:35 +0000

    Update Customer.java

--- src/OnlineRetailer/Customer.java
+++ src/OnlineRetailer/Customer.java
@@ -3,6 +3,7 @@ package OnlineRetailer;
 public class Customer {
 
     //Never changed
+    // ANDY - because you only use it inside one method.
     private final int customerID; //Why? "Can be converted to local variable?
 
     //Shared data
